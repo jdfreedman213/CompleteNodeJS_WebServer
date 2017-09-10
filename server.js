8cf3caefc3a14d2a997fb8089126cd94cfc9251e
Jeff Freedman <jeff@act2hq.com>
2017-09-10 11:52:11 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+/**
+ * Project: WebServer
+ * File: server.js
+ * Copyright (c) 2017, Jeff Freedman.
+ * Created by Jeff on 9/7/2017.
+ */
+'use strict';
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+		res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\r\n', (err) => {
+		if (err) {
+			console.error('Unable to append to server.log.');
+		}
+	});
+	next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my test site!'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Error handling this request'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
